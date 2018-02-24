cce24b7f914c97c5d9cf47385fc4c16f2fc84b7e
ТиесоваД <@343_1>
2018-02-24 14:25:18 +0600

    dchg

---
+++ ListEmployer/Employers.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ListEmployer
+{
+    [Serializable]
+   public class Employers
+    {
+        /* 3.Список всех сотрудников, должен находиться в отдельном XML файле и содержать следующие поля:
+     - ID сотрудника
+     - ФИО
+     - Должность
+     - и прочие основные поля, на выбор, но не более 6 полей*/
+       static int IDem = 0;
+     public int ID { get; set; }
+     public string Name { get; set; }
+     public string Proffession { get; set; }
+    
+        public int Salaire { get; set; }
+        public Employers(string Name, string Proffession,  int Salaire)
+        {
+           
+            this.Name = Name;
+            this.Proffession = Proffession;
+            this.Salaire = Salaire;
+        }
+
+        public Employers()
+        {
+        }
+
+     
+       
+       
+        public Employers CreateNew()
+        {
+          
+            Employers obj = new Employers();
+           
+            IDem++;
+            obj.ID = IDem;
+            Console.WriteLine("Name ");
+            
+            obj.Name = Console.ReadLine();
+            
+            Console.WriteLine("Proffession");
+            obj.Proffession = Console.ReadLine();
+            Console.WriteLine("Salaire ");
+            obj.Salaire = Int32.Parse(Console.ReadLine());
+
+            XmlSerializer formatter = new XmlSerializer(typeof(Employers));
+            using (FileStream fs = new FileStream("Empolyers_" +obj.ID+".xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, obj);
+                Console.WriteLine("Done empo");
+            }
+
+            return obj;
+        }
+      
+
+        public Employers UpdateOne()
+        {
+            Employers em = new Employers();
+            XmlSerializer formatter = new XmlSerializer(typeof(Employers));
+
+
+
+            Console.WriteLine("New Name ");
+
+            em.Name = Console.ReadLine();
+
+            Console.WriteLine("Proffession");
+            em.Proffession = Console.ReadLine();
+            Console.WriteLine("Salaire ");
+            em.Salaire = Int32.Parse(Console.ReadLine());
+
+           
+            using (FileStream fs = new FileStream("Empolyers_" + em.ID + ".xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, em);
+                Console.WriteLine("Done empo");
+            }
+
+            return em;
+
+        }
+
+        public void FindeInfo(int num)
+        {
+            
+
+        }
+
+    }
+}
---
+++ ListEmployer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ListEmployer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ListEmployer")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c0b95fc1-d6bc-4885-af3e-7e0c0ee260ab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
